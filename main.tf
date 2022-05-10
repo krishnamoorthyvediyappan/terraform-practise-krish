7a41140fc5290142cc54dcd6241ce7f786e76530
krishnamoorthyvediyappan <36813823+krishnamoorthyvediyappan@users.noreply.github.com>
2022-05-10 21:43:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 
 resource "aws_instance" "myec2"{
   ami                    = "ami-079b5e5b3971bd10d"
-  instance_type          = "t2.micro
+  instance_type          = "t2.micro"
 }
 
