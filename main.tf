e459c7f6fd2e544e93ac5d11f79e02ed9960473a
krishnamoorthyvediyappan <36813823+krishnamoorthyvediyappan@users.noreply.github.com>
2022-05-10 21:39:16 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  profile="default"
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "myec2"{
+  ami                    = "ami-079b5e5b3971bd10d"
+  instance_type          = "t2.micro
+}
+
